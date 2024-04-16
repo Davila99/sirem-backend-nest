d0477207dc85de412782505f07fc950d2c117be1
Davila99 <davilaeliseo453@gmail.com>
2024-04-15 20:47:01 -0600

    modulo de asignaturas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CargosModule } from './cargos/cargos.module';
 import { ProfesionesModule } from './profesiones/profesiones.module';
+import { GradosModule } from './grados/grados.module';
+import { AsignaturasModule } from './asignaturas/asignaturas.module';
 
 @Module({
-  imports: [CargosModule, ProfesionesModule],
+  imports: [CargosModule, ProfesionesModule, GradosModule, AsignaturasModule],
   controllers: [AppController],
   providers: [AppService],
 })
