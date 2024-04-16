3176ad6a7e553a59878975d6440bf790f01590f3
Davila99 <davilaeliseo453@gmail.com>
2024-04-15 20:54:42 -0600

    modelo secciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { CargosModule } from './cargos/cargos.module';
 import { ProfesionesModule } from './profesiones/profesiones.module';
 import { GradosModule } from './grados/grados.module';
 import { AsignaturasModule } from './asignaturas/asignaturas.module';
+import { TurnosModule } from './turnos/turnos.module';
+import { SeccionesModule } from './secciones/secciones.module';
 
 @Module({
-  imports: [CargosModule, ProfesionesModule, GradosModule, AsignaturasModule],
+  imports: [CargosModule, ProfesionesModule, GradosModule, AsignaturasModule, TurnosModule, SeccionesModule],
   controllers: [AppController],
   providers: [AppService],
 })
